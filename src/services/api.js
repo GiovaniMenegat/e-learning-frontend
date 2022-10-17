43a93428bf87adcb0043e3464c83598669a1ba55
GiovaniMenegat <gi.menegat10@gmail.com>
2022-10-17 19:40:51 -0300

    fix: add api https route

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: process.env.NODE_ENV === "development" ? "http://localhost:3001" : "http://54.232.34.108:3001",
+ baseURL: process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://54.232.34.108.nip.io",
 });
 
 export default api
